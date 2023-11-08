c4806a31ec84b2ad8927d6090f5a75b994171966
geisa <geisanebiu1@gmail.>
2023-11-08 18:10:02 +0100

    working on auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import Auth from "./components/Auth";
+import "./App.css";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  console.log(isLoggedIn);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{!isLoggedIn ? <Auth /> : <div>o pune</div>}</div>
   );
 }
 
